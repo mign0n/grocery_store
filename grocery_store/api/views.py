0270bd6c7a14b70eee78b5e5c3fca922c1ed3f96
mign0n <mign0n.git@gmail.com>
2024-07-15 18:13:56 +0500

    feat: Add /cart/empty endpoint.

--- grocery_store/api/views.py
+++ grocery_store/api/views.py
@@ -1,5 +1,8 @@
 from django.db.models import QuerySet
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.request import Request
+from rest_framework.response import Response
 
 from api import serializers
 from api.permissions import ReadOnly
@@ -24,3 +27,8 @@ class CartViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self) -> QuerySet:
         return Cart.objects.filter(owner=self.request.user).all()
+
+    @action(methods=('DELETE',), detail=False)  # type: ignore
+    def empty(self, request: Request) -> Response:
+        self.get_queryset().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
